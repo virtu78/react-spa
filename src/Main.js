7552686127e5d1636c16996a65f239e2fcf38761
virtu78 <virtu78@yandex.ru>
2019-11-12 16:09:32 +0500

    initial commit

---
+++ src/Main.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react"
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import PrivateRoute from './PrivateRoute';
+import Home from './pages/Home';
+import Admin from './pages/Admin';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import { AuthContext } from './context/auth';
+
+
+function Main(props) {
+	const [authTokens, setAuthTokens] = useState();
+	
+	const setTokens = (data) => {
+    localStorage.setItem("tokens", JSON.stringify(data));
+    setAuthTokens(data);
+  }
+		return(
+		
+			<AuthContext.Provider value={{ authTokens, setAuthTokens: setTokens }}>
+				<Router>
+					<div>
+						<h1>Задачник</h1>
+						<ul className="header">
+							<li ><Link to="/login">Вход</Link></li>
+							<li><Link to="/">Задачи</Link></li>
+							<li><Link to="/admin">Редактировать задачи</Link></li>
+							
+							
+						</ul>
+						<div >
+							<Route exact path="/" component={Home}/>
+							<Route path="/login" component={Login} />
+							<Route path="/signup" component={Signup} />
+							<PrivateRoute path="/admin" component={Admin}/>					
+						</div>
+					</div>
+				</Router>
+			</AuthContext.Provider>
+		);
+}
+	
+export default Main;
